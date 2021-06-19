3e03792dd5274abac311efcd7db18baf030104ea
terotero57 <86070986+terotero57@users.noreply.github.com>
2021-06-19 21:39:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
-from bokeh.models import DataTable, TableColumn, HTMLTemplateFormatter
 
 df = pd.read_csv('category.csv')
 
@@ -13,14 +10,6 @@ genre=st.sidebar.radio(
 
 df=df[df["Category"]==genre]
 
-cds = ColumnDataSource(df)
-columns = [
-TableColumn(field="Category", title="Category"),
-TableColumn(field="Maker", title="Maker"),
-TableColumn(field="pedal", title="pedal"),
-TableColumn(field="URL", title="URL", formatter=HTMLTemplateFormatter(template='<a href="<%= URL %>"target="_blank"><%= value %></a>')),
-]
-p = DataTable(source=cds, columns=columns, css_classes=["my_table"])
 #st.sidebar.bokeh_chart(p)
 j=len(df)
 
